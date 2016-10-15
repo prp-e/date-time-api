0dccd2e19fb673771233124552b10cbc24393231
Muhammadreza Haghiri <haghiri75@gmail.com>
2016-10-15 15:54:45 +0330

    Conversion added

--- api.rb
+++ api.rb
@@ -21,3 +21,11 @@ end
 get '/date/parse/:current' do
  "#{Time.parse(params[:current]).month()} / #{Time.parse(params[:current]).day()} / #{Time.parse(params[:current]).year()} , #{Time.parse(params[:current]).hour} : #{Time.parse(params[:current]).min()} : #{Time.parse(params[:current]).sec()} , Zone : #{Time.parse(params[:current]).zone()}"
 end
+
+get '/date/to/jalali/:current' do
+ "#{Date.parse(params[:current]).to_jalali.day} / #{Date.parse(params[:current]).to_jalali.month} / #{Date.parse(params[:current]).to_jalali.year} "
+end
+
+get '/date/to/gregorian/:current' do
+ "#{Parsi::Date.parse(params[:current]).to_gregorian.month} / #{Parsi::Date.parse(params[:current]).to_gregorian.day } / #{Parsi::Date.parse(params[:current]).to_gregorian.year}"
+end
